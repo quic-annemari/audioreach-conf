f89c5eacd9d52f9cb59883910e314c7cd5409137
ffrancis <ffrancis@qti.qualcomm.com>
2025-08-04 16:05:10 +0530

    CI: Add checkers and pre-merge workflows
    - Introduce checkers and pre-merge workflows using shared workflow from
    https://github.com/Audioreach/audioreach-workflows
    
    - Replace Repolinter with multi_checker in .github/workflows.
    
    - Add scripts and build configuration in ~ci/ directory used by the
    centralized workflow:
      - build.sh: Contains build commands.
      - build_options.txt: Lists recipe dependencies.
      - files_to_copy.sh: Manages source-to-target artifact copying.
    
    Signed-off-by: ffrancis <ffrancis@qti.qualcomm.com>

---
+++ ci/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+#
+# Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved.
+set -ex
+echo "Running build script..."
+
+# Build/Compile audioreach-conf
+# make sure we are in the right directory
+cd ${GITHUB_WORKSPACE}
+source ${GITHUB_WORKSPACE}/install/environment-setup-armv8-2a-poky-linux
+mkdir ${GITHUB_WORKSPACE}/build
+mkdir ${GITHUB_WORKSPACE}/build/etc
+mkdir ${GITHUB_WORKSPACE}/build/etc/acdbdata
+mkdir ${GITHUB_WORKSPACE}/build/etc/acdbdata/qcm6490_rb3
+mkdir ${GITHUB_WORKSPACE}/build/etc/acdbdata/qcs8300
+mkdir ${GITHUB_WORKSPACE}/build/etc/acdbdata/qcs9075
+mkdir ${GITHUB_WORKSPACE}/build/etc/acdbdata/qcs9100
+
+cp -r qcom/qcm6490/card-defs.xml ${GITHUB_WORKSPACE}/build/etc/
+cp -r qcom/qcm6490/acdbdata/* ${GITHUB_WORKSPACE}/build/etc/acdbdata/qcm6490_rb3/
+cp -r qcom/qcs8300/acdbdata/* ${GITHUB_WORKSPACE}/build/etc/acdbdata/qcs8300/
+cp -r qcom/qcs9100/acdbdata/* ${GITHUB_WORKSPACE}/build/etc/acdbdata/qcs9100/
+cp -r qcom/qcs9075/acdbdata/* ${GITHUB_WORKSPACE}/build/etc/acdbdata/qcs9075/
